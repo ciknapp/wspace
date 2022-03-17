1594893ec4fb0db7ab61abf062ca9e342fd65b4b
ciknapp <caleb.knapp@pm.me>
2022-03-17 12:22:35 -0500

    Remade project into console application

---
+++ wspace/WSFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wspace
+{
+    public class WSFile
+    {
+        public const string Extension = "ws";
+
+        public const string GeneratedCodeFolderName = "Generated Whitespace Code";
+
+        public WSFile(string name = "", string fileDir = "")
+        {
+            if (string.IsNullOrEmpty(name.Trim()))
+            {
+                Name = $"Whitespace{DateTime.Now.ToString("HHmmssyyyyMMdd")}.{Extension}";
+            }
+            else
+            {
+                Name = $"{name}.{Extension}";
+            }
+
+            if (string.IsNullOrEmpty(fileDir.Trim()))
+            {
+                if(Directory.Exists($"{Directory.GetCurrentDirectory()}\\{GeneratedCodeFolderName}") == false)
+                {
+                    Directory.CreateDirectory($"{Directory.GetCurrentDirectory()}\\{GeneratedCodeFolderName}");
+                }
+
+                FullFileDir = $"{Directory.GetCurrentDirectory()}\\{GeneratedCodeFolderName}\\{name}";
+            }
+            else
+            {
+                FullFileDir = $"{fileDir}\\{name}";
+            }
+        }
+
+        internal void Finish()
+        {
+            FileContents += "\n\n\n";
+
+            File.WriteAllText(FullFileDir, FileContents);
+        }
+
+        public string FileContents { get; set; }
+
+        public string FullFileDir { get; }
+
+        public string Name { get; }
+    }
+}
---
+++ wspace/Whitespace.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace wspace
+{
+    public static class Whitespace
+    {
+        public static bool IsInitialized { get => isInitialized; }
+        private static bool isInitialized = false;
+
+        public  static WSFile WSFile { get => wSFile; }
+        private static WSFile wSFile;
+
+        public static void InitializeWhitespace(string name = "", string fileDir = "")
+        {
+            wSFile = new WSFile(name, fileDir);
+
+            isInitialized = true;
+        }
+
+        public static void FinishProgram()
+        {
+            wSFile.Finish();
+        }
+
+        public static class Stack
+        {
+            public static void PushNumber(int number)
+            {
+                char[] binaryString = Convert.ToString(number, 2).ToCharArray();
+
+                for(int bit = 0; bit < binaryString.Length; bit++)
+                {
+                    char currentBit = binaryString[bit];
+
+                    binaryString[bit] = currentBit == '1' ? '\t' : ' ';
+                }
+
+                bool negative = number < 0;
+
+                wSFile.FileContents += $"  {(negative ? "\t" : " ")}{binaryString}";
+            }
+        }
+    }
+}
